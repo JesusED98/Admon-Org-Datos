58cb91f6a60debd5db6adafab4484950ee4cfac9
Jesus Estrada <60419443+JesusED98@users.noreply.github.com>
2020-11-02 23:05:57 -0600

    Add files via upload

---
+++ practica3_13/ejemplo2.php
@@ -0,0 +1,9 @@
+<?php 
+//arreglo normal -indexado 
+$autos = array("Volvo", "BMW", "Toyota"); 
+
+//convertimos arreglo asociativo en json 
+$mijson = json_encode($autos); 
+
+//mostrammos variable 
+echo $mijson;
